483203288d76108cb08b90b5df69d20fd5806e61
Mário Nunes <mariogar1979@gmail.com>
2018-08-24 13:02:50 -0300

    Melhorado a saida, e detecão de suporte ao CPUID

--- src/cpuid/cpuid.asm
+++ src/cpuid/cpuid.asm
@@ -1,23 +1,44 @@
 section .bss
-    vendor_id resd 12
+
+section .data
+    output:     db  "The processor Vendor ID is '"
+    .regEBXvalue:    db      "xxxx"
+    .regEDXvalue:    db      "xxxx"
+    .regECXvalue:    db      "xxxx"
+                     db      "'", 10
+    .length:         equ     $-output
+    nosupport:  db  "CPUID is not supported", 10
+    .length:    equ $-nosupport
 
 section .text
     global _start
 
 _start:
 
-    nop
-    mov eax, 0
-    cpuid
-    mov [vendor_id], ebx
-    mov [vendor_id+4], edx
-    mov [vendor_id+8], ecx
+    pushfd
+    pop eax
+    mov ecx,eax
+    xor eax,0x200000
+    push eax
+    popfd
+    pushfd
+    pop eax
+    xor eax,ecx
+    shr eax,21
+    and eax,1
+    push ecx
+    popfd
+    jz .notSupported
 
-    mov edx, 12
-    mov ecx, vendor_id
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
+    xor eax,eax
+    cpuid
+    mov [output.regEBXvalue], ebx
+    mov [output.regEDXvalue], edx
+    mov [output.regECXvalue], ecx
+
+    mov edx, output.length
+    mov ecx, output
+    jmp .write
 
     mov edx, 1
     mov ecx, 0x0a
@@ -25,7 +46,15 @@ _start:
     mov eax, 4
     int 0x80
 
-
+    .notSupported:
+    mov edx, nosupport.length
+    mov ecx, nosupport
+    
+    .write:
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+    
     mov eax, 1
     mov ebx, 0
     int 0x80
