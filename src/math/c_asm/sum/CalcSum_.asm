f6234d8cfcc8b29f1a399e76a9fcdce22e18de64
Mário Nunes <mariogar1979@gmail.com>
2019-01-26 01:00:25 -0200

    Adicionado os comentários ao código fonte

--- src/math/c_asm/sum/CalcSum_.asm
+++ src/math/c_asm/sum/CalcSum_.asm
@@ -1,15 +1,19 @@
 section .text
 GLOBAL	CalcSum_
 CalcSum_:
+; Inicializa o Stack Frame Pointer
 	push ebp
 	mov ebp,esp	
 
-	mov eax,[ebp+8]
-	mov ecx,[ebp+12]
-	mov edx,[ebp+16]
+; Carrega os valores nos argumentos
+	mov eax,[ebp+8]		; eax = 'a'
+	mov ecx,[ebp+12]	; ecx = 'b'
+	mov edx,[ebp+16]	; edx = 'c'
 
-	add eax, ecx
-	add eax, edx
+; Calcular a soma
+	add eax, ecx		; eax = 'a' + 'b'
+	add eax, edx		; eax = 'a' + 'b' + 'c'
 
+; Restaura o Stack Frame Pointer do chamador.
 	pop ebp
 	ret
