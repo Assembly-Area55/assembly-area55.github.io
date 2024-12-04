f506a92c6f037c516e63b7a636dab752ac4ba6ff
Mario Augusto Souza Nunes <mario.nunes@zello.tec.br>
2024-12-03 22:26:16 -0300

    Código comentado e adicionado segundo exemplo usando parametros.

---
+++ src/uppercase/uppercase.asm
@@ -0,0 +1,49 @@
+; Compilação:
+; nasm -f elf uppercase.asm
+;
+; ld -m elf_i386 -s -o uppercase uppercase.o
+;
+; Uso:
+; Crie um arquivo com um texto aleatório, e execute:
+; cat arquivo.txt | ./uppercase 
+
+section .bss
+        Buff resb 1
+
+section .data
+
+section .text
+        global _start
+
+_start:
+        nop
+
+leitura:
+        mov eax, 3
+        mov ebx, 0
+        mov ecx, Buff
+        mov edx, 1
+        int 80h
+
+        cmp eax, 0
+        je sair
+
+        cmp byte [Buff], 61h
+        jb escreve
+        cmp byte [Buff], 7ah
+        ja escreve
+
+        sub byte [Buff], 20h
+
+escreve:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, Buff
+        mov edx, 1
+        int 80h
+        jmp leitura
+
+sair:
+        mov eax, 1
+        mov ebx, 0
+        int 80h
