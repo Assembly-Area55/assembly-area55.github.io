04801434e94a9fe462311b82d2232d4ee72879ef
Mario Augusto Souza Nunes <mario.nunes@zello.tec.br>
2024-12-03 22:25:01 -0300

    Melhorias no código a fim de deixar mais didático

--- src/detect_vm/detect.asm
+++ src/detect_vm/detect.asm
@@ -3,20 +3,39 @@
 %include "vmware.inc"
 
 section .data
-  msg db 'Testa se esta rodando em um VM', 0x0a
+  msg db 'Testa se esta rodando em uma VM', 0x0a
   msg.len equ $ - msg
 
   msg.detect db 'VMware Detectado', 0x0a
   msg.detectlen equ $ - msg.detect
 
+  msg.not_detect db 'VMware não detectado', 0x0a
+  msg.not_detectlen equ $ - msg.detect
+
 section .text
   global _start
 
 _start:
-mov edx, msg.len
-mov ecx, msg
-call sprint
+    mov edx, msg.len
+    mov ecx, msg
+    call sprint
 
-call detect_vmware
+    call detect_vmware
+    cmp eax, 1
+    je vmware_detected
+    jmp not_vmware_detected
 
-call quit
+vmware_detected:
+  mov edx, msg.detectlen
+  mov ecx, msg.detect
+  call sprint
+  jmp end_program
+
+not_vmware_detected:
+  mov edx, msg.not_detectlen
+  mov ecx, msg.not_detect
+  call sprint
+  call quit
+
+end_program:
+  call quit
